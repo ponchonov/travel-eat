c41e4725bb46ae82fbdcf25e9a0ff3726a77cd80
Poncho Cuevas <poncho_nov@hotmail.com>
2019-08-21 12:11:09 -0500

    project created

---
+++ Travel&Eat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Travel&Eat
+//
+//  Created by Héctor Cuevas on 21/08/19.
+//  Copyright © 2019 Héctor Cuevas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Travel&EatTests/Travel_EatTests.swift
@@ -0,0 +1,34 @@
+//
+//  Travel_EatTests.swift
+//  Travel&EatTests
+//
+//  Created by Héctor Cuevas on 21/08/19.
+//  Copyright © 2019 Héctor Cuevas. All rights reserved.
+//
+
+import XCTest
+@testable import Travel_Eat
+
+class Travel_EatTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
